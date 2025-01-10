4959b1d1f344c0c3d045994954f2a560361b8926
FrancoEspinozaV <142062208+FrancoEspinozaV@users.noreply.github.com>
2025-01-09 23:44:16 -0300

    watch docker

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -4,10 +4,8 @@ import connectDB from '../src/database/db'
 
 const port = PORT || 4000
 
-// Conectar a la base de datos
 connectDB()
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando http://localhost:${port}`)
 })
