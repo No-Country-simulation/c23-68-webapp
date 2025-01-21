e7aafeb48072f6fa92e00756b3b335c819d4566a
FrancoEspinozaV <142062208+FrancoEspinozaV@users.noreply.github.com>
2025-01-21 01:43:29 -0300

    add deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/c23-68-webapp/',
   plugins: [react()],
-  css:  {
+  css: {
     preprocessorOptions: {
       css: {
         additionalData: `@import "./src/index.css";`,
