637c96bc36fd5a6c2f4cac24468776181a34ccdc
FrancoEspinozaV <142062208+FrancoEspinozaV@users.noreply.github.com>
2025-01-21 01:49:02 -0300

    roll back

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/c23-68-webapp/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
