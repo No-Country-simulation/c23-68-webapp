97d0499c268fdccc817bdb889e7de3be141ecc55
fabri <fabriciodanielturrina@gmail.com>
2025-01-23 19:00:45 -0300

    conflicts resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,13 @@
 import Navbar from "./components/layout/Navbar";
-import { nameModal } from "./config/nameModals";
 import { BrowserRouter, Router, useRoutes } from "react-router-dom";
-import usePopups from "./hooks/usePopups";
-import About from "./pages/About";
-import Finblog from "./pages/Finblog";
 import Home from "./pages/Home";
-import Team from "./pages/Team";
+
 
 function App() {
-  const { LoadingModalID, LoginModalID } = nameModal;
-  const { show } = usePopups();
-
-  const handleShowModal = (idModal) => {
-    show({
-      popUpId: idModal,
-      metadata: { id: idModal },
-      pushMethod: "prepend",
-    });
-  };
+ 
   const AppRoutes = () => {
     let routes = useRoutes([
       { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/finblog", element: <Finblog /> },
-      { path: "/team", element: <Team /> }
     ]);
     return routes;
   };
@@ -35,21 +19,7 @@ function App() {
         <AppRoutes />
       </BrowserRouter>
 
-      {/* <div className='flex flex-col items-center justify-center h-screen gap-4'>
-        <h1 className='text-xl text-red-600'> Testing de modales</h1>
-        <button
-          className='px-2 py-1 border border-red-400 rounded-lg'
-          onClick={() => handleShowModal(LoadingModalID)}
-        >
-          Show Loading Modal
-        </button>
-        <button
-          className='px-2 py-1 border border-red-400 rounded-lg'
-          onClick={() => handleShowModal(LoginModalID)}
-        >
-          Show Login Modal
-        </button> 
-      </div>  */}
+      
     </>
   );
 }
