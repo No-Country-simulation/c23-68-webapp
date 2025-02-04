2ad890ea644d7f9ad0a7f9ef373771cdff1d8bd2
Luis Angel <angel.quispe.navarro@outlook.com>
2025-02-04 11:33:57 -0500

    Vista-Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
       <div className="bg-white shadow-lg rounded-3xl border-b-8  border-r-8 border-l-2 border-t-2 border-black py-12 p-24 w-[89%] max-w-[1400px] my-16 mx-auto">
         <div className="text-center">
           <button className="bg-amarilloclaro font-normal font-onest text-xl px-7 py-2 rounded-full border border-black ">
-            Dashboard
+            Vista de Dashboard
           </button>
           <h2 className="text-4xl py-4 font-semibold font-anybody mt-4">
             Dashboard
