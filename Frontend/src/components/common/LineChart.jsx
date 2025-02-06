a8bbb92bbf973adfd32e51ecf39047d42cb4a180
FrancoEspinozaV <142062208+FrancoEspinozaV@users.noreply.github.com>
2025-02-06 05:27:29 -0300

    refactor: standardize formatting and improve readability in LineChart and Dashboard components

--- Frontend/src/components/common/LineChart.jsx
+++ Frontend/src/components/common/LineChart.jsx
@@ -1,18 +1,16 @@
-import { Card, LineChart, List, ListItem } from '@tremor/react';
+import { Card, LineChart, List, ListItem } from '@tremor/react'
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(' ')
 }
 
 const data = [
   {
-    date: "2025-02-05",
+    date: '2025-02-05',
     income: 4745,
-    expense: 2467
-},
-
-
-];
+    expense: 2467,
+  },
+]
 
 const summary = [
   {
@@ -23,27 +21,26 @@ const summary = [
     name: 'expense',
     value: 120,
   },
-];
+]
 
 const valueFormatter = (number) =>
-  `${Intl.NumberFormat('us').format(number).toString()}`;
+  `${Intl.NumberFormat('us').format(number).toString()}`
 
 const statusColor = {
   income: 'bg-emerald-500',
   expense: 'bg-pink-600',
-};
-
+}
 
 export default function Example() {
   return (
     <>
-      <Card className=" pl-12 pr-16 py-8 w-[60%] h-auto bg-white rounded-3xl shadow-lg font-onest">
-        <h3 className="text-lg text-negro font-semibold">
-        Comparación entre gastos e ingresos
+      <Card className=' pl-12 pr-16 py-8 w-[60%] h-auto bg-white rounded-3xl shadow-lg font-onest'>
+        <h3 className='text-lg font-semibold text-negro'>
+          Comparación entre gastos e ingresos
         </h3>
         <LineChart
           data={data}
-          index="date"
+          index='date'
           categories={['income', 'expense']}
           colors={['emerald-500', 'pink-600']}
           valueFormatter={valueFormatter}
@@ -51,26 +48,31 @@ export default function Example() {
           showYAxis={true}
           showXAxis={false}
           startEndOnly={true}
-          className="mt-6 h-40 bg-white p-4"
+          className='h-40 p-4 mt-6 bg-white'
         />
-        <List className="mt-2 flex justify-center -space-x-64">
+        <List className='flex justify-center mt-2 -space-x-64'>
           {summary.map((item) => (
-            <ListItem key={item.name} className="flex flex-col items-center space-x-1">
-              <div className="flex items-center space-x-2">
+            <ListItem
+              key={item.name}
+              className='flex flex-col items-center space-x-1'
+            >
+              <div className='flex items-center space-x-2'>
                 <span
-                  className={classNames(statusColor[item.name], 'h-0.5 w-3 py-2 px-2 rounded-sm')}
+                  className={classNames(
+                    statusColor[item.name],
+                    'h-0.5 w-3 py-2 px-2 rounded-sm'
+                  )}
                   aria-hidden={true}
                 />
                 <span>{item.name}</span>
               </div>
-              <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+              <span className='font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong'>
                 {valueFormatter(item.value)}
               </span>
             </ListItem>
           ))}
         </List>
       </Card>
-      
     </>
-  );
+  )
 }
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,52 +1,49 @@
-import React from "react";
-import {Card, Title } from "@tremor/react";
-import "../styles/Home.css";
-import LineChart from "../components/common/LineChart";
-import DonutChart from "../components/common/DonutChart";
-import BarChart from "../components/common/BarChart";
+import { Card, Title } from '@tremor/react'
+import '../styles/Home.css'
+import LineChart from '../components/common/LineChart'
+import DonutChart from '../components/common/DonutChart'
+import BarChart from '../components/common/BarChart'
 
 const Dashboard = () => {
   return (
-    <div className="font-onest bg-gray-100 flex flex-col min-h-screen pt-3 px-6">
+    <div className='flex flex-col min-h-screen px-6 pt-3 bg-gray-100 font-onest'>
       {/* Encabezado */}
-      <div className="flex items-center w-[95%] max-w-[1400px] mt-6">
-        <h2 className="ml-[6%] text-3xl text-negro font-normal">
+      <div className='flex items-center w-[95%] max-w-[1400px] mt-6'>
+        <h2 className='ml-[6%] text-3xl text-negro font-normal'>
           ¡Buenos Días, Samir!
         </h2>
         <img
-          src="/images/dashboard.png"
-          alt="Logo"
-          className="inline-block ml-3"
+          src='/images/dashboard.png'
+          alt='Logo'
+          className='inline-block ml-3'
         />
       </div>
 
       {/* Tarjetas de resumen */}
-      <div className=" pl-10 flex justify-between mt-10">
+      <div className='flex justify-between pl-10 mt-10 '>
         <LineChart />
-        <div className="flex flex-col gap-4 mt-auto w-[30%] ml-auto mr-10">
-          <Card className="bg-verde text-white text-center rounded-3xl p-5 shadow-lg">
-            <Title className="text-2xl">Ingreso Total</Title>
-            <p className="text-2xl">$2,200.00</p>
+        <div className='flex flex-col gap-4 mt-auto w-[30%] ml-auto mr-10'>
+          <Card className='p-5 text-center text-white shadow-lg bg-verde rounded-3xl'>
+            <Title className='text-2xl'>Ingreso Total</Title>
+            <p className='text-2xl'>$2,200.00</p>
           </Card>
-          <Card className="bg-rosa2 text-white text-center rounded-3xl p-5 shadow-lg">
-            <Title className="text-2xl">Gasto Total</Title>
-            <p className="text-2xl">$400.00</p>
+          <Card className='p-5 text-center text-white shadow-lg bg-rosa2 rounded-3xl'>
+            <Title className='text-2xl'>Gasto Total</Title>
+            <p className='text-2xl'>$400.00</p>
           </Card>
-          <Card className="bg-amarillooscuro text-white text-center rounded-3xl p-5 shadow-lg">
-            <Title className="text-2xl">Dinero Restante</Title>
-            <p className="text-2xl">$1,800.00</p>
+          <Card className='p-5 text-center text-white shadow-lg bg-amarillooscuro rounded-3xl'>
+            <Title className='text-2xl'>Dinero Restante</Title>
+            <p className='text-2xl'>$1,800.00</p>
           </Card>
         </div>
       </div>
 
-      <div className=" pl-10 flex justify-between mt-10 pr-10 ">
-      <DonutChart />
-      <BarChart />
+      <div className='flex justify-between pl-10 pr-10 mt-10 '>
+        <DonutChart />
+        <BarChart />
       </div>
-
     </div>
-    
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
